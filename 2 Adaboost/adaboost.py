256e3f2467ee0c1434686dc7ccd96e4893c221f3
abk <abk@wlan-145-94-155-168.wlan.tudelft.nl>
2017-03-08 19:03:14 +0100

    3.8 night
    size for the digit dataset can be adjusted

--- 2 Adaboost/adaboost.py
+++ 2 Adaboost/adaboost.py
@@ -14,7 +14,9 @@ def new_sign(x):
         return -1
         
 
-def min_cut(cut,array,label,p):
+def cut_error(cut,array,label,p):
+    #calculate the cut error
+    #implement 2 error variable in case making completely opposite classification
     error1 = 0
     error2 = 0
     for i in list(range(np.size(array))):
@@ -26,9 +28,7 @@ def min_cut(cut,array,label,p):
         if label[i]*(cut - array[i]) <= 0:
             error2 = error2 + p[i]      
             
-            #implement 2 error variable in case making completely opposite classification
-            #we take the smaller one to return the error
-    #print(cut,error1,error2)
+        #we take the smaller one to return the error
     if error1 <= error2:
         order = -1  #means small - -1, large - 1
         return error1, order
@@ -51,15 +51,15 @@ def get_cut(data):
 def stump(data,label,p):
     #make sure it update at the first time
     min_error = 100000
-    step_length = 0.01
     for i in list(range(np.size(data.T[0]))):#i indicates the dimension(feature)
         #make sure we scan it thoroughly
         cuts = get_cut(data[i])
         #adjust the step length here
-        
+        #print("i=",i)
         for j in cuts:#j goes through one feature of all samples
-            error, order = min_cut(j,data[i],label,p)
+            error, order = cut_error(j,data[i],label,p)
             #get min error and corresponding dimension and cut position
+            #print("j=",j)
             if error < min_error:
                 min_error = error
                 min_cut_dimension = i
@@ -78,8 +78,8 @@ def get_err_index(array,label,cut,order):
             err_index[i] = 1
     
     #if more than 1/2 samples are misclassified, we just change the labels
-    if sum(err_index) > 0.5 * np.size(err_index):
-        err_index = 1 - err_index
+    #if sum(err_index) > 0.5 * np.size(err_index):
+    #    err_index = 1 - err_index
         
     return err_index
    
@@ -88,13 +88,13 @@ def get_ada(data,label,rounds):
     data0 = []
     data1 = []
     for i in range(0,np.size(data[0])):
-        if label[i] == -1:
+        if label[i] < 0:
             data0.append(data[:,i])
         else:
             data1.append(data[:,i])
     data0 = np.array(data0).T
     data1 = np.array(data1).T
-    #choose which two dimensions to visualize
+    #the visualization here is for 2d data
     plt.scatter(data0[0],data0[1],c='r')
     plt.scatter(data1[0],data1[1],c='b')
     
@@ -127,7 +127,7 @@ def get_ada(data,label,rounds):
 def ada_classifier(dim,cut,beta,order,observation):
     sum_decision = 0
     for i in list(range(np.size(dim))):
-        sum_decision = sum_decision - math.log(1/beta[i],2)*order[i]*np.sign(cut[i]-observation[dim[i]])
+        sum_decision = sum_decision + math.log(1/beta[i],2)*order[i]*np.sign(cut[i]-observation[dim[i]])
         #print(i,"vote:",sum_decision," beta:",beta[i])
     return new_sign(sum_decision)
     
@@ -147,18 +147,23 @@ def test_ada(data, label, list_dim, list_cut, list_beta, list_order):
 #generate gaussian data
 m1 = [0,0]
 m2 = [2,2]
-cov = [[0.4,0],[0,0.4]]
+cov = [[1,0],[0,1]]
 N = 20
-data0 = np.random.multivariate_normal(m1, cov, N).T
-data1 = np.random.multivariate_normal(m2, cov, N).T
-plt.scatter(data0[0],data0[1],c='r')
-plt.scatter(data1[0],data1[1],c='b')
+train0 = np.random.multivariate_normal(m1, cov, N).T
+train1 = np.random.multivariate_normal(m2, cov, N).T
+test0 = np.random.multivariate_normal(m1, cov, N).T
+test1 = np.random.multivariate_normal(m2, cov, N).T
+
+plt.scatter(train0[0],train0[1],c='r')
+plt.scatter(train1[0],train1[1],c='b')
 
 #set labels as -1 and 1
-data = np.append(data0,data1,axis=1)
+train_data = np.append(train0,train1,axis=1)
+test_data = np.append(test0,test1,axis=1)
 label1 = np.zeros(N)-1
 label2 = np.ones(N)
-label = np.append(label1,label2,axis=0)
+train_label = np.append(label1,label2,axis=0)
+test_label = np.append(label1,label2,axis=0)
 
 
 
@@ -172,6 +177,9 @@ label = np.append(label1,label2,axis=0)
 
 data = []
 
+train_size = 70
+test_size = 30
+
 for l in open("opt.txt"):
     row = [int(x) for x in l.split()]
     if len(row) > 0:
@@ -179,49 +187,30 @@ for l in open("opt.txt"):
         
 data0 = np.array(data[0:554])
 data1 = np.array(data[554:1125])
-train0 = data0[0:50].T
-train1 = data1[0:50].T
 
-data = np.append(train0,train1,axis=1)
-label1 = np.zeros(50)-1
-label2 = np.ones(50)
-label = np.append(label1,label2,axis=0)
+train0 = data0[0:train_size].T
+train1 = data1[0:train_size].T
+train_data = np.append(train0,train1,axis=1)
+label1 = np.zeros(train_size)-1
+label2 = np.ones(train_size)
+train_label = np.append(label1,label2,axis=0)
 
-#%% simple data
+test0 = data0[train_size:train_size+test_size].T
+test1 = data1[train_size:train_size+test_size].T
+test_data = np.append(test0,test1,axis=1)
+label1 = np.zeros(test_size)-1
+label2 = np.ones(test_size)
+test_label = np.append(label1,label2,axis=0)
+
+#%% simple data(6 samples)
 data0 = np.array([[1,1,2],[1,2,2]])
 data1 = np.array([[2,3,3],[1,1,2]])
 data = np.append(data0,data1,axis=1)
 label = np.array([-1,-1,-1,1,1,1])
-#%% Adaboost
-#
-#plt.scatter(data0[0],data0[1],c='r')
-#plt.scatter(data1[0],data1[1],c='b')
-#
-#weight = np.ones(np.size(data[0]))/np.size(data[0])
-#list_dim = []
-#list_cut = []
-#list_beta = []
-#list_order = []
-#
-#for i in list(range(10)):
-#    p = weight/sum(weight)
-#    dim,cut,error,order = stump(data,label,p)
-#    err_index = get_err_index(data[dim],label,cut,order)
-#    weight = weight * (error/(1-error))**(1-err_index)
-#    print(": dim:",dim," cut:",cut," error:",error," order:",order)
-#    #plot decision boundary
-#    if dim == 0:
-#        plt.axvline(x=cut)
-#    else:
-#        plt.axhline(y=cut)
-#  
-# #  list_dim.append(dim)
-#    list_cut.append(cut)
-#    list_beta.append(error/(1-error))
-#    list_order.append(order)
-#    
-#%% main
-list_dim, list_cut, list_beta, list_order = get_ada(data,label,10)
-#%%
-accuracy = test_ada(data,label,list_dim, list_cut, list_beta, list_order)
+
+#%% generate ada classifier
+list_dim, list_cut, list_beta, list_order = get_ada(train_data,train_label,10)
+#%% test
+accuracy = test_ada(test_data,test_label,list_dim, list_cut, list_beta, list_order)
     
+
